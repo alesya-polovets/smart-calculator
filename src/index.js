36e42e324e92d934820df619983bc9cafa451373
Alesya Polovets <alesya.polovets@gmail.com>
2018-09-29 16:50:46 +0300

    smart-calculator/feature: implemented class SmartCalculator

--- src/index.js
+++ src/index.js
@@ -1,27 +1,35 @@
 class SmartCalculator {
   constructor(initialValue) {
     // your implementation
+      this.initialValue = String(initialValue);
   }
-
   add(number) {
     // your implementation
+      this.initialValue = this.initialValue + '+' + number;
+      return this;
   }
-  
   subtract(number) {
     // your implementation
+      this.initialValue = this.initialValue + '-' + number;
+      return this;
   }
-
   multiply(number) {
     // your implementation
+      this.initialValue = this.initialValue + '*' + number;
+      return this;
   }
-
   devide(number) {
     // your implementation
+      this.initialValue = this.initialValue + '/' + number;
+      return this;
   }
-
   pow(number) {
     // your implementation
+      this.initialValue = this.initialValue + '**' + number;
+      return this;
+  }
+  toString() {
+      return eval(this.initialValue);
   }
 }
-
 module.exports = SmartCalculator;
